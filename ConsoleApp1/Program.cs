724fa9b702535066882f20ad11bd15aeccdf64b1
Lennartajansson <lennart.a.jansson@outlook.com>
2023-07-03 16:23:25 +0200

    Changed result into a dictionary

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,16 +2,19 @@
 using System.Reflection;
 
 Test test1 = new() { P1 = 10, P2 = 20, P3 = "Adam" };
-Test test2 = new() { P1 = 10, P2 = 20, P3 = "Adam" };
+Test test2 = new() { P1 = 10, P2 = 20, P3 = "Nisse" };
 
-Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1")));
-Console.WriteLine(!test1.PropertiesEqual(test2, "P1").Any());
-Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1", "P2")));
-Console.WriteLine(!test1.PropertiesEqual(test2, "P1", "P2").Any());
-Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1", "P2", "P3")));
-Console.WriteLine(!test1.PropertiesEqual(test2, "P1", "P2", "P3").Any());
+IDictionary<string, Diff> test = test1.PropertiesEqual(test2, "P1");
+Console.WriteLine(!test.Any());
+test = test1.PropertiesEqual(test2, "P1", "P2");
+Console.WriteLine(!test.Any());
+test = test1.PropertiesEqual(test2, "P1", "P2", "P3");
+Console.WriteLine(!test.Any());
 
 
+
+IEnumerable<KeyValuePair<string, Diff>> nisse = test.AsEnumerable();
+Console.WriteLine(nisse);
 public class Test
 {
     public int P1 { get; set; }
@@ -21,26 +24,36 @@ public class Test
 
 public static class TestExtensions
 {
-    public static IEnumerable<string> PropertiesEqual(this Test t1, Test t2, params string[] propertyNames)
+    public static IDictionary<string, Diff> PropertiesEqual(this Test t1, Test t2, params string[] propertyNames)
     {
         Type? typ = t1.GetType();
-        List<string> propertyDiff = new();
+        Dictionary<string, Diff> propertyDiff = new();
 
         foreach (PropertyInfo property in typ.GetProperties().Where(p => propertyNames.Contains(p.Name)))
         {
             PropertyInfo? pa = typ.GetProperties().SingleOrDefault(p => p.Name.Equals(property.Name));
-            object? o1 = pa.GetValue(t1);
-            object? o2 = pa.GetValue(t2);
 
-            if (o1 is not null && o2 is not null)
+            if (pa is not null)
             {
-                if (!o1.Equals(o2))
+                object? o1 = pa.GetValue(t1);
+                object? o2 = pa.GetValue(t2);
+
+                if (o1 is not null && o2 is not null)
                 {
-                    propertyDiff.Add(property.Name);
+                    if (!o1.Equals(o2))
+                    {
+                        propertyDiff.Add(property.Name, new Diff() { Source = o1, Target = o2 });
+                    }
                 }
             }
         }
 
         return propertyDiff;
     }
+}
+
+public class Diff
+{
+    public required object Source { get; set; }
+    public required object Target { get; set; }
 }
