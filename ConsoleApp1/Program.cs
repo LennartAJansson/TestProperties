074e399332a6be2562bc987df68377564ce6060b
Lennartajansson <lennart.a.jansson@outlook.com>
2023-07-03 15:48:24 +0200

    Using Equal for comparison

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Reflection;
 
-Test test1 = new() { P1 = 10, P2 = 20, P3 = 30 };
-Test test2 = new() { P1 = 10, P2 = 30, P3 = 40 };
+Test test1 = new() { P1 = 10, P2 = 20, P3 = "Adam" };
+Test test2 = new() { P1 = 10, P2 = 20, P3 = "Adam" };
 
-Console.WriteLine(string.Join(',', test1.IsPropertiesEqual(test2, "P1")));
-Console.WriteLine(!test1.IsPropertiesEqual(test2, "P1").Any());
-Console.WriteLine(string.Join(',', test1.IsPropertiesEqual(test2, "P1", "P2")));
-Console.WriteLine(!test1.IsPropertiesEqual(test2, "P1", "P2").Any());
-Console.WriteLine(string.Join(',', test1.IsPropertiesEqual(test2, "P1", "P2", "P3")));
-Console.WriteLine(!test1.IsPropertiesEqual(test2, "P1", "P2", "P3").Any());
+Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1")));
+Console.WriteLine(!test1.PropertiesEqual(test2, "P1").Any());
+Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1", "P2")));
+Console.WriteLine(!test1.PropertiesEqual(test2, "P1", "P2").Any());
+Console.WriteLine(string.Join(',', test1.PropertiesEqual(test2, "P1", "P2", "P3")));
+Console.WriteLine(!test1.PropertiesEqual(test2, "P1", "P2", "P3").Any());
 
 
 public class Test
 {
     public int P1 { get; set; }
     public int P2 { get; set; }
-    public int P3 { get; set; }
+    public required string P3 { get; set; }
 }
 
 public static class TestExtensions
 {
-    public static IEnumerable<string> IsPropertiesEqual(this Test t1, Test t2, params string[] propertyNames)
+    public static IEnumerable<string> PropertiesEqual(this Test t1, Test t2, params string[] propertyNames)
     {
         Type? typ = t1.GetType();
         List<string> propertyDiff = new();
@@ -29,12 +29,15 @@ public static class TestExtensions
         foreach (PropertyInfo property in typ.GetProperties().Where(p => propertyNames.Contains(p.Name)))
         {
             PropertyInfo? pa = typ.GetProperties().SingleOrDefault(p => p.Name.Equals(property.Name));
-            int o1 = (int)pa.GetValue(t1);
-            int o2 = (int)pa.GetValue(t2);
+            object? o1 = pa.GetValue(t1);
+            object? o2 = pa.GetValue(t2);
 
-            if (o1 != o2)
+            if (o1 is not null && o2 is not null)
             {
-                propertyDiff.Add(property.Name);
+                if (!o1.Equals(o2))
+                {
+                    propertyDiff.Add(property.Name);
+                }
             }
         }
 
